0666c9dcfd055b2f00ab9b9916c2028f3439b66f
Amber Dostert <55667404+Adostert@users.noreply.github.com>
2020-05-29 09:14:59 -0400

    Second commit- Jquery API works

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
     $("button").on("click", getDJ);
 });
-
 function getDJ() {
-
-
-    $.get("https://icanhazdadjoke.com/", function (json) {
-        // var data = JSON.parse(JSON.stringify(json));
-        console.log(json);
-        //     $(".joke").html(data.joke);
+    $.ajax({
+        url: "https://icanhazdadjoke.com/",
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            format: 'json'
+        },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Accept', 'application/json');
+        },
+        success: function(data) {
+            $('.joke').html(data.joke);
+        },
+        error: function(xhr, status, error) {
+            console.log('there was an error')
+        }
     });
 }
 
